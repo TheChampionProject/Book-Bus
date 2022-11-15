bb57e324d7cab3c5ed1b79a25620385927b19ccc
0marA <theengineermachine@gmail.com>
2022-11-14 18:44:05 -0600

    Connection to Google Books and BooksRun

--- backend/apiController.js
+++ backend/apiController.js
@@ -1,9 +1,15 @@
 import asyncHandler from "express-async-handler";
 import { getBooksFB, setBookFB } from "./firebase.js";
+import dotenv from "dotenv";
 import axios from "axios";
+dotenv.config();
+
+let ISBN;
 
 const GOOGLE_BOOKS_API_BASE_URL =
     "https://www.googleapis.com/books/v1/volumes?q=";
+
+const BOOKS_RUN_API_BASE_URL = `https://booksrun.com/api/v3/price/buy/0439887453?key=${process.env.BOOKS_RUN_API_KEY}`;
 
 const getBooks = asyncHandler(async (req, res) => {
     res.send(await getBooksFB());
@@ -29,7 +35,16 @@ const searchForBook = asyncHandler(async (req, res) => {
     let searchRequest = await axios.get(
         GOOGLE_BOOKS_API_BASE_URL + req.body.title
     );
-    console.log(searchRequest.data.items[0].volumeInfo);
+
+    ISBN =
+        searchRequest.data.items[0].volumeInfo.industryIdentifiers[0]
+            .identifier;
+
+    console.log(ISBN);
+
+    let price = await axios.get(BOOKS_RUN_API_BASE_URL);
+
+    console.log(price.data.result.offers.booksrun);
 });
 
 export { getBooks, setBook, searchForBook };
