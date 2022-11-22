5815e1c49838d3d0b26d626da922853980988737
0marA <theengineermachine@gmail.com>
2022-11-21 17:44:34 -0800

    Cool gift message

--- frontend/src/components/GiftConfirmation.js
+++ frontend/src/components/GiftConfirmation.js
@@ -24,7 +24,7 @@ export default function GiftConfirmation({
                 </Modal.Header>
 
                 <Modal.Body>
-                    <h3>Would you like to gift {book.Title}?</h3>
+                    <h4>Would you like to gift {book.Title}?</h4>
                     <Button
                         variant="secondary"
                         className="btn btn-danger GiftButtons"
